7ac9750b4ead7cf4926cace0f91c4a5c89f58df8
someoneex <nishitchaudhary350@gmail.com>
2025-05-16 17:25:44 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
-bot_token = getenv("bot_token", "")
+api_id    = int(getenv("api_id", "26797881"))
+api_hash  = getenv("api_hash", "9699262c708c2e45ba18bfce925ed5ed")
+bot_token = getenv("bot_token", "7973139452:AAHs02tcFE7ooK4SlnKtxqX3igcw14Jli4c")
 pwtoken = getenv("pwtoken", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-AUTH = ""
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002579668223"))
+AUTH = "6567162029"
 #Bot Username = ""
